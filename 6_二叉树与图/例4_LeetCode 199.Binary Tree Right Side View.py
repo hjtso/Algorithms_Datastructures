e98e6b226a73762eb32beb9d768a546b3253dc3f
H.J.T <562784738@qq.com>
2022-09-30 18:46:30 +0900

    Add files via upload

--- 6_二叉树与图/例4_LeetCode 199.Binary Tree Right Side View.py
+++ 6_二叉树与图/例4_LeetCode 199.Binary Tree Right Side View.py
@@ -5,7 +5,7 @@
 #         self.left = None
 #         self.right = None
 import collections
-
+# collections.deque()库
 class Solution(object):
     def rightSideView(self, root):
         """
@@ -27,3 +27,74 @@ class Solution(object):
         return res
 
 # https://blog.csdn.net/fuxuemingzhu/article/details/79557632
+
+# queue.Queue()库
+# class Solution(object):
+#     def rightSideView(self, root):
+#         """
+#         :type root: TreeNode
+#         :rtype: List[int]
+#         """
+#         if root is None:
+#             return []
+#         res = self.BFS_travle(root)
+#         fin = []
+#         for value in res.values():
+#             fin.append(value[-1])
+#         return fin
+#
+#     def BFS_travle(self, root):
+#         from queue import Queue
+#         queue = Queue()
+#         queue.put((0, root))
+#         sum_dict = {}
+#         while not queue.empty():
+#             (level, node) = queue.get()
+#             try:
+#                 sum_dict[level].append(node.val)
+#             except KeyError:
+#                 sum_dict[level] = []
+#                 sum_dict[level].append(node.val)
+#
+#             if node.left:
+#                 queue.put((level + 1, node.left))
+#             if node.right:
+#                 queue.put((level + 1, node.right))
+#
+#         return sum_dict
+
+
+
+# 只适用于完全二叉树
+# class Solution(object):
+#     def rightSideView(self, root):
+#         """
+#         :type root: TreeNode
+#         :rtype: List[int]
+#         """
+#         if root is None:
+#             return []
+#         res = self.BFS_travle(root)
+#         fin = []
+#         for value in res.values():
+#             fin.append(value[-1])
+#         return fin
+#
+#     def BFS_travle(self, root):
+#         sum_dict = {}
+#         index = 0
+#         List = [root]
+#         while List:
+#             q = List.pop(0)
+#             try:
+#                 sum_dict[int(math.log2(index+1))].append(q.val)
+#             except KeyError:
+#                 sum_dict[int(math.log2(index+1))] = []
+#                 sum_dict[int(math.log2(index+1))].append(q.val)
+#             if q.left != None:
+#                 List.append(q.left)
+#             if q.right != None:
+#                 List.append(q.right)
+#             index += 1
+#         print(sum_dict)
+#         return sum_dict
